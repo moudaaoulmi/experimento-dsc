5e7e9a96e96509721fbf7e9b6a60660b69d7acf3
chambinha <chambinha@a509e640-e7ea-11dd-b00f-9fabb4b2ff13>
2009-02-10 23:19:31 +0000

    Cristiane - revisão

--- src/com/atlassw/tools/eclipse/checkstyle/util/table/TableHandler.java
+++ src/com/atlassw/tools/eclipse/checkstyle/util/table/TableHandler.java
@@ -8,10 +8,5 @@ public class TableHandler extends GeneralException {
     public void restoreState2Handler(int mSortDirection, int DIRECTION_FORWARD){
         mSortDirection = DIRECTION_FORWARD;
     }
-    
-    public void restoreState3Handler(boolean allColumnsHaveStoredData ){
-        // probably a new column
-        allColumnsHaveStoredData = false;
-    }
-        
+       
 }
