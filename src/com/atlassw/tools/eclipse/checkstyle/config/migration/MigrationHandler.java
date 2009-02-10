4328da7904bd2537a2a596701ec291bd054dbe8c
chambinha <chambinha@a509e640-e7ea-11dd-b00f-9fabb4b2ff13>
2009-02-10 22:50:41 +0000

    Cristiane - Segundo nível reuso

--- src/com/atlassw/tools/eclipse/checkstyle/config/migration/MigrationHandler.java
+++ src/com/atlassw/tools/eclipse/checkstyle/config/migration/MigrationHandler.java
@@ -1,31 +1,18 @@
 
 package com.atlassw.tools.eclipse.checkstyle.config.migration;
 
-import java.io.OutputStream;
 import java.text.DateFormat;
 import java.util.Date;
 
-import org.apache.commons.io.IOUtils;
 import org.eclipse.osgi.util.NLS;
-import org.xml.sax.SAXException;
 
 import com.atlassw.tools.eclipse.checkstyle.Messages;
 import com.atlassw.tools.eclipse.checkstyle.config.CheckConfigurationWorkingCopy;
+import com.atlassw.tools.eclipse.checkstyle.exception.GeneralException;
 import com.atlassw.tools.eclipse.checkstyle.util.CheckstylePluginException;
 
-public class MigrationHandler
+public class MigrationHandler extends GeneralException
 {
-    public void checkConfigurationMigratorMigrate(SAXException se) throws CheckstylePluginException
-    {
-        Exception ex = se.getException() != null ? se.getException() : se;
-        CheckstylePluginException.rethrow(ex);
-    }
-
-    public void checkConfigurationMigratorCheckstylePluginException(Exception e)
-        throws CheckstylePluginException
-    {
-        CheckstylePluginException.rethrow(e);
-    }
 
     public void checkConfigurationMigratorStartElement(
             CheckConfigurationWorkingCopy mCurrentConfiguration, String name)
@@ -37,17 +24,6 @@ public class MigrationHandler
                 .bind(Messages.CheckConfigurationMigrator_txtMigrationAddition, DateFormat
                         .getDateTimeInstance(DateFormat.FULL, DateFormat.FULL).format(new Date()));
         mCurrentConfiguration.setName(name + nameAddition);
-    }
-
-    public void checkConfigurationMigratorSAXException(CheckstylePluginException e)
-        throws SAXException
-    {
-        throw new SAXException(e);
-    }
-
-    public void checkConfigurationMigratorEnsureFileExists(OutputStream out)
-    {
-        IOUtils.closeQuietly(out);
     }
 
 }
